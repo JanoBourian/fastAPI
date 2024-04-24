f90ac6315669bfc45c08c87dfbb547c6b8b43f04
JanoBourian <supermath33@icloud.com>
2024-04-24 14:54:44 -0600

    add: first structure

--- README.md
+++ README.md
@@ -5,3 +5,49 @@
 ```bash
 docker run -d --name inavona -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_USER=myusername -e PGDATA=/var/lib/postgresql/data/pgdata -p 5434:5432 -v C:\\Users\\super\\Documents\\databases\\inavona:/var/lib/postgresql/data -d postgres
 ```
+
+Packages: 
+* alembic
+* sqlalchemy
+* databases
+* python-dotenv
+* asyncpg
+* psycopg2
+* psycopg2-binary
+
+## Database configuration
+
+The basic and first step, but in the future we will work with migrations (Alembic)
+
+```python
+import databases
+import sqlalchemy
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values('.env')
+}
+
+DATABASE_URL = '%s://%s:%s@%s:%s/%s'%(
+    config.get("DBDRIVER"),
+    config.get("DBUSERNAME"),
+    config.get("DBPASSWORD"),
+    config.get("DBHOST"),
+    config.get("DBPORT"),
+    config.get("DBNAME")
+)
+
+data = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+books = sqlalchemy.Table(
+    "books",
+    metadata,
+    sqlalchemy.Column("uuid", sqlalchemy.Uuid, primary_key=True),
+    sqlalchemy.Column("title", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("author", sqlalchemy.String, nullable=False),
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
+```
---
+++ main.py
@@ -0,0 +1,57 @@
+from contextlib import asynccontextmanager
+import uuid
+import asyncio
+import databases
+import sqlalchemy
+from dotenv import dotenv_values
+from fastapi import FastAPI, Request
+
+config = {
+    **dotenv_values('.env')
+}
+
+DATABASE_URL = '%s://%s:%s@%s:%s/%s'%(
+    config.get("DBDRIVER"),
+    config.get("DBUSERNAME"),
+    config.get("DBPASSWORD"),
+    config.get("DBHOST"),
+    config.get("DBPORT"),
+    config.get("DBNAME")
+)
+
+data = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+books = sqlalchemy.Table(
+    "books",
+    metadata,
+    sqlalchemy.Column("uuid", sqlalchemy.Uuid, primary_key=True, unique=True),
+    sqlalchemy.Column("title", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("author", sqlalchemy.String, nullable=False),
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await data.connect()
+    yield
+    await data.disconnect()
+    await asyncio.sleep(2)
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/books/")
+async def get_all_books():
+    query = books.select()
+    return await data.fetch_all(query)
+
+@app.post("/books/")
+async def create_book(request:Request):
+    uuid_value = uuid.uuid4()
+    body = await request.json()
+    body.update({'uuid': uuid_value})
+    query = books.insert().values(**body)
+    await data.execute(query)
+    return {"client": uuid_value}
